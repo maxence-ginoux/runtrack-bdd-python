8152c712573f07220219e86fd2f882947865615f
maxence.ginoux <maxence.ginoux@laplateforme.io>
2024-01-29 16:45:21 +0100

    ajout de Martin et affichage des étudiants DUPUIS

---
+++ jour01/job12/job12.sql
@@ -0,0 +1,6 @@
+ajout de Martin Dupuis à la table :
+insert into etudiant values (6, "Dupuis", "Martin", 18, "martin.dupuis@laplateforme.io");
+
+affichage des etudiants "DUPUIS" :
+select * from etudiant
+where nom = "Dupuis";
