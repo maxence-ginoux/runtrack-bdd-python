52246041b890216c1357f3d7966f5ab0cae475ab
maxence.ginoux <maxence.ginoux@laplateforme.io>
2024-01-30 15:32:04 +0100

    compter le nombre d'étudiants ayant entre 18 et 25 ans

---
+++ jour01/job20/job20.sql
@@ -0,0 +1,2 @@
+Compter le nombre d'étudiants de -18 ans:
+SELECT COUNT(*) AS nombre_etudiants_mineurs FROM etudiant WHERE age < 18;
---
+++ jour01/job21/job21.sql
@@ -0,0 +1,2 @@
+compter le nombre d'étudiants ayant entre 18 et 25 ans:
+SELECT COUNT(*) AS "nombre d'étudiants entre 18 et 25 ans" FROM etudiant WHERE age BETWEEN 18 AND 25;
